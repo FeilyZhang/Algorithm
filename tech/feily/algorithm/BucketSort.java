7e8c6c3c5f152abe099e0720371aff4ad312e3a7
Feily Zhang <fei472625@foxmail.com>
2018-10-31 00:03:02 +0800

    桶排序算法实现

---
+++ tech/feily/algorithm/BucketSort.java
@@ -0,0 +1,29 @@
+package tech.feily.algorithm;
+
+public class BucketSort {
+
+    private int[] buckets;
+    private int[] array;
+
+    public BucketSort(int range, int[] array) {
+        this.buckets = new int[range];
+        this.array = array;
+    }
+
+    public void sort() {
+        if (array != null && array.length > 1) {
+            for (int i = 0; i < array.length; i++) {
+                buckets[array[i]] ++;
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = buckets.length - 1; i >= 0; i--) {
+            for (int j = 0; j < buckets[i]; j++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
