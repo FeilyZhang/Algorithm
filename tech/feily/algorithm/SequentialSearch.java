e5967cc22016fb1302b4b698653b46d3717c8ec6
张鹏飞 <fei472625@foxmail.com>
2018-11-03 00:18:27 +0800

    搜索算法，顺序查找

---
+++ tech/feily/algorithm/SequentialSearch.java
@@ -0,0 +1,21 @@
+package tech.feily.algorithm;
+
+public class SequentialSearch {
+
+    private int[] array;
+
+    public SequentialSearch(int[] array) {
+        this.array = array;
+    }
+
+    public int search(int value) {
+        int len = array.length;
+        for (int i = 0; i < len; i++) {
+            if (value == array[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
